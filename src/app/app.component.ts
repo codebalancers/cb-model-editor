2697ac56c2b22defff52f4e8b42e2f1b0b865134
Arne Bosien <info@arnebosien.de>
2018-05-17 12:15:05 +0200

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div class='row'>
+      <div #editor class='medium-12 columns'></div>
+    </div>
+  `,
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('editor') editor: ElementRef;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    console.log(this.editor.nativeElement);
+
+    const _editor = new JSONEditor(this.editor.nativeElement, {
+      // Enable fetching schemas via ajax
+      ajax: true,
+
+      theme: 'bootstrap4',
+      iconlib: 'fontawesome4',
+
+      // The schema for the editor
+      schema: {
+        $ref: 'api/schema.json',
+        format: 'grid'
+      }
+
+      // Seed the form with a starting value
+      // startval: starting_value
+    });
+  }
 }
